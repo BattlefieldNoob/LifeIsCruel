079626469605cfa5f08818c747d2ee32d4f872b6
Alessandro Buonpane <ale.buonpane@gmail.com>
2017-03-22 12:16:18 +0100

    pugnos

---
+++ Assets/Scripts/Pugno.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pugno : MonoBehaviour
+{
+
+    public float maxScale;
+    public float minScale;
+    public float punchTime;
+    float actualPunchTime; 
+    public float waitTime;
+    bool punching;
+    public bool going; 
+    public void Punch()
+    {
+        if (!punching)
+            StartCoroutine(PunchCoroutine());
+    }
+
+    IEnumerator PunchCoroutine()
+    {
+        punching = true;
+        float counter = 0;
+        actualPunchTime = punchTime * Random.Range(0.75f, 1.5f); 
+        float apex = actualPunchTime / 2;
+        while (counter <= actualPunchTime)
+        {
+            float a = (maxScale - minScale) / apex * waitTime;
+            if (counter <= apex)
+            {
+                going = true; 
+                Gonfia(a);
+            }
+            else
+            {
+                going = false; 
+                Sgonfia(a);
+            }
+            counter += waitTime;
+            yield return new WaitForSeconds(waitTime);
+        }
+        transform.localScale = new Vector3(minScale, minScale, 0);
+        punching = false;
+    }
+
+    void Gonfia(float amount)
+    {
+        transform.localScale += new Vector3(amount, amount, 0);
+    }
+
+    void Sgonfia(float amount)
+    {
+        transform.localScale -= new Vector3(amount, amount, 0);
+    }
+}
---
+++ Assets/Scripts/Puncher.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puncher : MonoBehaviour
+{
+
+    public GameObject ready;
+    public GameObject punch;
+    public GameObject parata;
+    public Sprite puchFace;
+    public Sprite parataFace;
+    public AudioClip bark;
+
+    public GameObject pugno_dx;
+    public GameObject pugno_sx;
+
+    bool b;
+
+    SpriteRenderer sp;
+    AudioSource aus;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sp == null)
+        {
+            sp = GetComponent<SpriteRenderer>();
+        }
+
+        if (aus == null)
+        {
+            aus = GetComponent<AudioSource>();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+            StartCoroutine(ContinuousPunch()); 
+    }
+
+    public void Punch()
+    {
+        b = !b; 
+        ready.SetActive(false);
+        pugno_sx.SetActive(true);
+        pugno_dx.SetActive(true);
+        if (b)
+        {
+            if (!pugno_sx.GetComponent<Pugno>().going)
+            {
+                Pugno pd = pugno_dx.GetComponent<Pugno>();
+                pd.Punch();
+            }
+        }
+        else
+        {
+            if (!pugno_dx.GetComponent<Pugno>().going)
+            {
+                Pugno ps = pugno_sx.GetComponent<Pugno>();
+                ps.Punch();
+            }
+        }
+
+        aus.PlayOneShot(bark);
+
+    }
+
+    IEnumerator ContinuousPunch()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.1f);
+            Punch();
+        }
+    }
+
+    public void Ready()
+    {
+        ready.SetActive(true);
+        pugno_dx.SetActive(false);
+        pugno_sx.SetActive(false);
+    }
+}
